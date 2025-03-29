7fddeb4ae432e9988f059e35ed3132d31a0aecaf
filleap93 <filleap@duck.go>
2025-03-29 14:38:02 +0100

    ✨ Enable CORS for /api/demo

--- src/main/java/fr/gouv/interieur/dso/controller/DemoController.java
+++ src/main/java/fr/gouv/interieur/dso/controller/DemoController.java
@@ -5,6 +5,7 @@ import fr.gouv.interieur.dso.service.jdbc.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class DemoController {
     @Autowired
     private DemoService demoService;
 
+    @CrossOrigin(origins = "*")  // Enable CORS for all origins
     @GetMapping("/")
     public ResponseDto helloWorld() {
         ResponseDto responseDto = new ResponseDto();
